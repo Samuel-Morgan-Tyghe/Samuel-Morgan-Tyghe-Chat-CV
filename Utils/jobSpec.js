87712d6f66e787238c2c124e4493268f1029169e
Sam-Morgan-Tyghe <sam.morgan@hedgehoglab.com>
2023-07-04 14:58:21 +0100

    expand and add default job desc

--- Utils/jobSpec.js
+++ Utils/jobSpec.js
@@ -60,7 +60,33 @@ Knowledge and Experience
 • Knowledge of the data needs for FX and related financial markets would be an advantage.
 To apply, please email dev-jobs@bidfx.com.`;
 
+const defaultGenericJobDesc = `
+We are looking for a skilled react.js developer to join our front-end development team. In this role, you will be responsible for developing and implementing user interface components using React.js concepts and workflows such as Redux, Flux, and Webpack. You will also be responsible for profiling and improving front-end performance and documenting our front-end codebase.
+
+To ensure success as a react.js developer, you should have in-depth knowledge of JavaScript and React concepts, excellent front-end coding skills, and a good understanding of progressive web applications. Ultimately, a top-class react.js developer should be able to design and build modern user interface components to enhance application performance.
+
+React.js Developer Responsibilities:
+Meeting with the development team to discuss user interface ideas and applications.
+Reviewing application requirements and interface designs.
+Identifying web-based user interactions.
+Developing and implementing highly responsive user interface components using react concepts.
+Writing application interface codes using JavaScript following react.js workflows.
+Troubleshooting interface software and debugging application codes.
+Developing and implementing front-end architecture to support user interface concepts.
+Monitoring and improving front-end performance.
+Documenting application changes and developing updates.
+React.js Developer Requirements:
+Bachelor’s degree in computer science, information technology, or a similar field.
+Previous experience working as a react.js developer.
+In-depth knowledge of JavaScript, CSS, HTML, and front-end languages.
+Knowledge of REACT tools including React.js, Webpack, Enzyme, Redux, and Flux.
+Experience with user interface design.
+Knowledge of performance testing frameworks including Mocha and Jest.
+Experience with browser-based debugging and performance testing software.
+Excellent troubleshooting skills.
+Good project management skills.`;
+
 export const getJobSpec = (jobId) =>
   ({
     bidfx: bidfx,
-  }[jobId] ?? "No job spec found");
+  }[jobId] ?? defaultGenericJobDesc);
