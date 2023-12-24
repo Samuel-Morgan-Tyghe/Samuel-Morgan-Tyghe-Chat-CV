698ba8ac4a05d42b94f234d0c42ead3d6a24c455
Sam-Morgan-Tyghe <sam.morgan@hedgehoglab.com>
2023-12-24 19:44:25 +0000

    update PDF name by date

--- components/DownloadCV.tsx
+++ components/DownloadCV.tsx
@@ -1,9 +1,26 @@
-import { Link } from '@chakra-ui/react';
+import { Link } from "@chakra-ui/react";
 
-const DownloadCV = () => (
-    <Link href="/Samuel_Morgan-Tyghe_CV.pdf" download color="blue.500">
-        Download My PDF CV
+const DownloadCV = () => {
+  // Generate a UK-friendly date string in the format DD-MM-YYYY
+  const date = new Date();
+  const formattedDate = [
+    ("0" + date.getDate()).slice(-2),
+    ("0" + (date.getMonth() + 1)).slice(-2),
+    date.getFullYear(),
+  ].join("-");
+
+  // Specify the download filename with the UK-friendly current date appended
+  const downloadFileName = `Samuel_Morgan-Tyghe_CV_${formattedDate}.pdf`;
+
+  return (
+    <Link
+      href="/Samuel_Morgan-Tyghe_CV.pdf"
+      download={downloadFileName}
+      color="blue.500"
+    >
+      Download My PDF CV
     </Link>
-);
+  );
+};
 
 export default DownloadCV;
